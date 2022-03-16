be1aa2ab6dec088c89f8c5a903340e893909a948
andrea1632 <andrea.bellucci10@libero.it>
2022-03-16 15:53:58 +0100

    snack3

---
+++ snack3/main.js
@@ -0,0 +1,13 @@
+let content = document.getElementById("contenitore");
+let numeriInt = [1, 2, 3, 4 , 5, 6, 7, 8, 9, 10];
+console.log(numeriInt);
+let somma = 0;
+let snitch = false;
+for (i = 1; i < numeriInt.length; i++){
+    if(numeriInt[i] % 2 != 0){
+       snitch = true
+       somma += numeriInt[i]
+    }
+}
+console.log(somma)
+content.innerHTML += `la somma è ${somma}`
